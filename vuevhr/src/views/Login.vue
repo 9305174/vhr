a4358090d80e675ce67e74855d99e00c976d018a
gaojian <9305174@qq.com>
2020-08-13 13:39:33 +0800

    1.登录的点击事件已完成，以及数据校验

--- vuevhr/src/views/Login.vue
+++ vuevhr/src/views/Login.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <el-form :rules="rules" :model="loginForm" class="loginContainer">
+        <el-form :rules="rules" ref="loginForm" :model="loginForm" class="loginContainer">
             <h3 class="loginTitle">系统登录</h3>
             <el-form-item  prop="username" >
                 <el-input type="text" v-model="loginForm.username" auto-complete="off" placeholder="请输入用户名"></el-input>
@@ -9,7 +9,7 @@
                 <el-input type="password" v-model="loginForm.password" auto-complete="off" placeholder="请输入密码"></el-input>
             </el-form-item>
             <el-checkbox class="loginRemember" v-model="checked">记住我</el-checkbox>
-            <el-button type="primary" style="width: 100%;">登录</el-button>
+            <el-button type="primary" style="width: 100%;" @click="submitLogin">登录</el-button>
         </el-form>
     </div>
 </template>
@@ -28,6 +28,19 @@
                     username:[{required:true,message:'请输入用户名',trigger:'blur'}],
                     password:[{required:true,message:'请输入密码',trigger:'blur'}]
                 }
+
+            }
+        },
+        methods:{
+            submitLogin(){
+                this.$refs.loginForm.validate((valid) => {
+                    if (valid) {
+                        alert('submit!');
+                    } else {
+                        this.$message.error('请输入全部字段!');
+                        return false;
+                    }
+                });
             }
         }
     }
